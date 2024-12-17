70f8c22a48aacf0ab37dc5b9c78d87c9dfe7a1be
Prasanth656 <cpprasanth5220@gmail.com>
2024-12-17 17:17:53 +0530

    First Time Push

---
+++ src/test/java/com/api/Single_Read.java
@@ -0,0 +1,21 @@
+package com.api;
+
+public class Single_Read {
+	
+	public void dob() {
+		float a=12.123456111f;
+		System.out.println("Float value:"+a);
+		
+	}
+	public void dou() {
+		double b=13.1234567891011122;
+		System.out.println("Doble value:"+b);
+				}
+public static void main(String[] args) {
+	Single_Read s=new Single_Read();
+	s.dob();
+	s.dou();
+	
+	
+}
+}
