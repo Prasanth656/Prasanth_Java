9290ff4e3e7f857a759c34a80d4b711998ef6db8
Prasanth656 <cpprasanth5220@gmail.com>
2024-12-18 08:18:09 +0530

    Class Added

---
+++ src/test/java/com/api/Simplegit.java
@@ -0,0 +1,16 @@
+package com.api;
+
+public class Simplegit {
+	public void m1() {
+System.out.println("1");
+	}
+	public void m2() {
+System.out.println("2");
+	}
+	public void m3() {
+System.out.println("3");
+	}
+	
+	
+
+}
